71a074b0c4639ca16c5fdf97c17745e5bcfe2435
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-08 11:45:56 -0500

    0.6.12 - bumped build depends

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, acid-state, attoparsec, base, blaze-html, cereal
+, clckwrks, containers, directory, filepath, gd, happstack-server
+, hsp, hsx2hs, ixset, magic, mtl, reform, reform-happstack
+, reform-hsp, safecopy, stdenv, text, web-plugins, web-routes
+, web-routes-th
+}:
+mkDerivation {
+  pname = "clckwrks-plugin-media";
+  version = "0.6.12";
+  src = ./.;
+  buildDepends = [
+    acid-state attoparsec base blaze-html cereal clckwrks containers
+    directory filepath gd happstack-server hsp ixset magic mtl reform
+    reform-happstack reform-hsp safecopy text web-plugins web-routes
+    web-routes-th
+  ];
+  buildTools = [ hsx2hs ];
+  homepage = "http://clckwrks.com/";
+  description = "media plugin for clckwrks";
+  license = stdenv.lib.licenses.bsd3;
+}
