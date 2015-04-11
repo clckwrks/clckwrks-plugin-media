b257baf6f3448deec9847754520602cf7238a10e
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-11 11:20:03 -0500

    added depend on C library file

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 , clckwrks, containers, directory, filepath, gd, happstack-server
 , hsp, hsx2hs, ixset, magic, mtl, reform, reform-happstack
 , reform-hsp, safecopy, stdenv, text, web-plugins, web-routes
-, web-routes-th
+, web-routes-th, file
 }:
 mkDerivation {
   pname = "clckwrks-plugin-media";
@@ -15,6 +15,7 @@ mkDerivation {
     web-routes-th
   ];
   buildTools = [ hsx2hs ];
+  extraLibraries = [ file ];
   homepage = "http://clckwrks.com/";
   description = "media plugin for clckwrks";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ let pkg = haskellngPackages.callPackage
              , clckwrks, containers, directory, filepath, gd, happstack-server
              , hsp, hsx2hs, ixset, magic, mtl, reform, reform-happstack
              , reform-hsp, safecopy, stdenv, text, web-plugins, web-routes
-             , web-routes-th
+             , web-routes-th, file
              }:
              mkDerivation {
                pname = "clckwrks-plugin-media";
@@ -17,6 +17,7 @@ let pkg = haskellngPackages.callPackage
                  web-routes-th
                ];
                buildTools = [ hsx2hs ];
+               extraLibraries = [ file ];
                homepage = "http://clckwrks.com/";
                description = "media plugin for clckwrks";
                license = stdenv.lib.licenses.bsd3;
