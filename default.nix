105aa676b27e0a69a1bb9ab276cfc927f3812710
Jeremy Shaw <jeremy@n-heptane.com>
2015-09-22 20:12:02 -0500

    0.6.14 - allow acid-state 0.13

--- default.nix
+++ default.nix
@@ -6,16 +6,16 @@
 }:
 mkDerivation {
   pname = "clckwrks-plugin-media";
-  version = "0.6.12";
+  version = "0.6.14";
   src = ./.;
-  buildDepends = [
+  libraryHaskellDepends = [
     acid-state attoparsec base blaze-html cereal clckwrks containers
     directory filepath gd happstack-server hsp ixset magic mtl reform
     reform-happstack reform-hsp safecopy text web-plugins web-routes
     web-routes-th
   ];
-  buildTools = [ hsx2hs ];
   extraLibraries = [ file ];
+  libraryToolDepends = [ hsx2hs ];
   homepage = "http://clckwrks.com/";
   description = "media plugin for clckwrks";
   license = stdenv.lib.licenses.bsd3;
