3461cd7c194924ca1d1f00a0353792237bd9dff0
Jeremy Shaw <jeremy@n-heptane.com>
2015-11-30 13:18:39 -0600

    .travis.yml: attempt to disable integer-simple flag when building text

--- shell.nix
+++ shell.nix
@@ -1,26 +1,37 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, acid-state, attoparsec, base, blaze-html, cereal
-             , clckwrks, containers, directory, filepath, gd, happstack-server
-             , hsp, hsx2hs, ixset, magic, mtl, reform, reform-happstack
-             , reform-hsp, safecopy, stdenv, text, web-plugins, web-routes
-             , web-routes-th, file
-             }:
-             mkDerivation {
-               pname = "clckwrks-plugin-media";
-               version = "0.6.12";
-               src = ./.;
-               buildDepends = [
-                 acid-state attoparsec base blaze-html cereal clckwrks containers
-                 directory filepath gd happstack-server hsp ixset magic mtl reform
-                 reform-happstack reform-hsp safecopy text web-plugins web-routes
-                 web-routes-th
-               ];
-               buildTools = [ hsx2hs ];
-               extraLibraries = [ file ];
-               homepage = "http://clckwrks.com/";
-               description = "media plugin for clckwrks";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, acid-state, attoparsec, base, blaze-html
+      , cereal, clckwrks, containers, directory, filepath, gd
+      , happstack-server, hsp, hsx2hs, ixset, magic, mtl, reform
+      , reform-happstack, reform-hsp, safecopy, stdenv, text, web-plugins
+      , web-routes, web-routes-th, cabal-install
+      }:
+      mkDerivation {
+        pname = "clckwrks-plugin-media";
+        version = "0.6.15";
+        src = ./.;
+        libraryHaskellDepends = [
+          acid-state attoparsec base blaze-html cereal clckwrks containers
+          directory filepath gd happstack-server hsp ixset magic mtl reform
+          reform-happstack reform-hsp safecopy text web-plugins web-routes
+          web-routes-th cabal-install
+        ];
+        libraryToolDepends = [ hsx2hs ];
+        homepage = "http://clckwrks.com/";
+        description = "media plugin for clckwrks";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
